50d0519e28806264b5e7622fad2f2f68b1e19c21
David Baxter <itsdavidbaxter@gmail.com>
2016-09-13 15:43:51 -0400

    parse synonymy csv file

--- make_taxon_table.pl
+++ make_taxon_table.pl
@@ -1,22 +1,52 @@
 #!usr/bin/perl
 use warnings;
 use strict;
+use Text::CSV;
 
 #declare input files
 my $taxon_id_file = "inputs/seaweed_taxon_ids.txt";
+my $synonymy_file = "inputs/names_synonyms.csv";
 my @content_files = ("KELP_species_pages.txt", "GREEN_species_pages.txt");
 
 #declare taxonID hash
 my %TID;
 
-#generate hash
+#generate taxon id hash %TID
 open(IN, $taxon_id_file) || die "couldn't find taxon id file $taxon_id_file\n";
 while(<IN>){
-	chomp;	
+	chomp;
 	my ($code,$name,$name_with_author)=split(/\t/);
 	$TID{$name}=$code;
 }
 close(IN);
+
+#parse synonymy file
+#declare hashes
+
+#parse synonymy csv file
+my $csv = Text::CSV->new({ sep_char => ',', quote_char => '"', escape_char => '\\'});
+open(IN, $synonymy_file) || die "couldn't find synonymy file file $synonymy_file\n";
+while(<IN>){
+	chomp;
+	if ($csv->parse($_)) {
+    	my @columns = $csv->fields();
+		unless($#columns==7){
+			warn "$#columns bad field number $_\n";
+			&log_issue("$#columns bad field number $_\n");
+		}
+		(my $syn_major_group,
+		my $syn_scientific_name,
+		my $syn_date,
+		my $syn_name_status,
+		my $syn_accepted_name,
+		my $syn_MAC_name,
+		my $syn_additions,
+		my $syn_non_native)=@columns;
+		print join ("\t",$syn_major_group,$syn_scientific_name,$syn_date,$syn_name_status,$syn_MAC_name,$syn_additions,$syn_non_native), "\n";
+	}
+	else{ warn "synonymy file parsing error $_\n"; }
+}
+die;
 
 #this function indicates the record delimiter. In this case, an empty line
 #as such, it needs to be designated after the other files are processed
@@ -118,4 +148,53 @@ sub log_issue {
 	open($log_file, '>>', $log_file);
 	print $log_file "logging: @_\n";
 	close $log_file;
+}
+
+sub strip_name{
+#subroutine adopted from CCH.pm, that removes the authority from a botanical name
+#e.g. converts "Acanthocladia muricata (Endlicher) Ruprecht" to "Acanthocladia muricata"
+	local($_) = @_;
+	s/× /X /g;
+	s/'//g;
+	s/`//g;
+	s/\?//g;
+	s/^ *//;
+	s/ *$//;
+	s/  +/ /g;
+	s/ spp\./ subsp./;
+	s/ssp\./subsp./;
+	s/ ssp / subsp. /;
+	s/ subsp / subsp. /;
+	s/ var / var. /;
+	s/ var. $//;
+	s/ sp\..*//;
+	s/ sp .*//;
+	s/ [Uu]ndet.*//;
+	s/ x / X /;
+	s/ . / X /;
+	s/ *$//;
+
+	s/ aff\. / /;
+	s/ cf\. / /;
+
+	s/Hook\. f./Hook./g;
+	s/Rech\. f./Rech./g;
+	s/Schult\. f./Schult./g;
+	s/Schultes f./Schultes/g;
+	s/Hallier f\./Hallier/g;
+	s/^([A-Z][A-Za-z]+) (X?[-a-z]+).*? (subvar\.|subsp\.|ssp\.|var\.|f\.) ([-a-z]+).*/$1 $2 $3 $4/ ||
+	s/^([A-Z][A-Za-z]+) X ([-a-z]+) .+/$1 X $2/||
+	s/^([A-Z][A-Za-z]+) × ?([-a-z]+) .+/$1 X $2/||
+	s/^([A-Z][A-Za-z]+) × ?([-a-z]+)/$1 X $2/||
+	s/^([A-Z][A-Za-z]+) (X?[-a-z]+) .+/$1 $2/||
+	s/^([A-Z][A-Za-z]+) (indet\.|sp\.)/$1 indet./||
+	s/^([A-Z][A-Za-z]+) (X?[-a-z]+)/$1 $2/||
+	s/^([A-Z][A-Za-z]+) (X [-a-z]+)/$1 $2/||
+	s/^X (A-Z][a-z]+) ([-a-z]+) (.+)/X $2 $3/||
+	s/^([A-Z][A-Za-z]+) (.+)/$1/;
+	s/ssp\./subsp./;
+	s/ +$//;
+	
+	#print "$_ \n";
+	$_;
 }
