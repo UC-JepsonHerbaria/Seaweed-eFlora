63064721d4dddfd178d9f32f824ee13cb814047c
David Baxter <itsdavidbaxter@gmail.com>
2016-09-13 18:48:42 -0400

    load synonym entries into database

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,2 +1,3 @@
 CREATE INDEX TaxonID ON eflora_taxa (TaxonID ASC);
 CREATE INDEX ScientificName ON eflora_taxa (ScientificName ASC);
+CREATE INDEX AcceptedNameTID ON eflora_taxa (AcceptedNameTID ASC);
---
+++ make_synonymy_table.pl
@@ -0,0 +1,141 @@
+#This script processes all lines from the names_synonyms.csv synonymy file
+#That are not accepted names, and prints out SQL to load them into the main table
+#With the AcceptedNameTID in the AcceptedNameTID field of the database
+#NOTE that the current workflow only processes names with treatments and not-accepted names
+#meaning that accepted names that do not have a species page treatment are not indexed
+
+#!usr/bin/perl
+use warnings;
+use strict;
+use Text::CSV;
+
+#declare input files
+my $taxon_id_file = "inputs/seaweed_taxon_ids.txt";
+my $synonymy_file = "inputs/names_synonyms.csv";
+
+#declare taxonID hash
+my %TID;
+
+#generate taxon id hash %TID
+open(IN, $taxon_id_file) || die "couldn't find taxon id file $taxon_id_file\n";
+while(<IN>){
+	chomp;
+	my ($code,$name,$name_with_author)=split(/\t/);
+	$TID{$name}=$code;
+}
+close(IN);
+
+#open output file
+my $output_file = "outputs/load_synonymy_table.sql";
+open(OUT, ">outputs/load_synonymy_table.sql") || die;
+
+#parse synonymy csv file
+my $csv = Text::CSV->new({ sep_char => ',', quote_char => '"'});
+open(IN, $synonymy_file) || die "couldn't find synonymy file file $synonymy_file\n";
+my $header_line=<IN>;   #skip header line
+while(<IN>){
+	chomp;
+	
+	#Escape single quotes for SQL insert
+	s/'/''/g;
+	
+	if ($csv->parse($_)) {
+    	my @columns = $csv->fields();
+		unless($#columns==7){
+			warn "$#columns bad field number $_\n";
+			&log_issue("$#columns bad field number $_\n");
+		}
+		my ($syn_major_group,
+		$syn_scientific_name_author,
+		$syn_date,
+		$syn_name_status,
+		$syn_accepted_name,
+		$syn_MAC_name,
+		$syn_additions,
+		$syn_non_native)=@columns;
+		
+		#skip all accepted names
+		next if $syn_name_status=~/accepted/i;
+		#clean up name status
+		$syn_name_status=~s/ *$//;
+		$syn_name_status=~s/s$//;
+		
+		#get and format scientificname
+		$syn_scientific_name_author=~s/ *$//;
+		my $syn_scientific_name=&strip_name($syn_scientific_name_author);
+		#$syn_accepted_name=&strip_name($syn_accepted_name);
+		$syn_accepted_name=~s/  +/ /;
+		$syn_accepted_name=~s/^ *//;
+		$syn_accepted_name=~s/ *$//;
+
+		
+		#get $AcceptedNameTID and confirm that it exists
+		my $AcceptedNameTID = $TID{$syn_accepted_name};
+		unless ($AcceptedNameTID){
+			warn "no taxon id for accepted name $syn_accepted_name\tfor synonym $syn_scientific_name\nadd $syn_accepted_name to seaweed_taxon_ids.txt\n";
+			&log_issue("no taxon id for $syn_accepted_name\t$syn_scientific_name");
+			next;
+		}
+		
+		#format other fields to be printed to SQL insert statement
+		$syn_major_group=&format_for_SQL($syn_major_group);
+		$syn_name_status=&format_for_SQL($syn_name_status);
+		$syn_date=&format_for_SQL($syn_date);
+		$syn_additions=&format_for_SQL($syn_additions);
+		
+		#record native status as it is recorded for accepted names
+		my $native_status;
+		if ($syn_non_native) {
+			$native_status = "'NON-NATIVE'";
+		}
+		else { $native_status = "NULL"; }
+		
+		#format scientific name for SQL before printing
+		$syn_scientific_name = "'$syn_scientific_name'";
+
+		
+		#print SQL insert statement to output
+		print OUT "INSERT INTO eflora_taxa(ScientificName, NativeStatus, NameStatus, DescriptionDate, MajorGroup, Additions, AcceptedNameTID)\n";
+		print OUT "VALUES($syn_scientific_name, $native_status, $syn_name_status, $syn_date, $syn_major_group, $syn_additions, $AcceptedNameTID)\n";
+		print OUT ";\n";
+	}
+	
+	else { 
+		warn "synonymy file parsing error $_\n";
+	}
+}
+
+############################
+sub strip_name {
+	local($_) = @_;
+	s/^([A-Z][-a-z]+) (X?[-a-z]+).*(subsp\.|ssp\.|var\.|f\.) ([-a-z]+).*/$1 $2 $3 $4/ ||
+	s/^([A-Z][a-z]+) ([x ]*[-a-z]+).*/$1 $2/;
+	s/ssp\./subsp./;
+
+	s/^ *//;
+	s/ *$//;
+	s/  */ /;
+	return ($_);
+}
+
+
+sub format_for_SQL {
+	my($input) = @_;
+	if ($input=~/.+/){
+		my $content = $input;
+		$content=~s/ *$//;
+		return "\'$content\'";
+	}
+	else {
+		return "NULL";
+	}
+}
+
+sub log_issue {
+#prints the inputted message to the designated log file
+	my $log_file = 'outputs/log.txt';
+	no strict "refs";
+	open($log_file, '>>', $log_file);
+	print $log_file "logging: @_\n";
+	close $log_file;
+}
