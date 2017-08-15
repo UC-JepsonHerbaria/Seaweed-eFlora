8089817c254ebf0c896903699728ac8cb4a42058
jalexander <jason_alexander@berkeley.edu>
2017-08-15 09:43:45 -0700

    updated links with new website directories

--- common/php/seaweed_menu.php
+++ common/php/seaweed_menu.php
@@ -1,10 +1,16 @@
 <div id='cssglobalmenu'>
 <ul>
-	<li class='active'><a href='http://ucjeps.berkeley.edu/seaweedflora/seaweedflora/'><span>Home</span></a></li>
-	<li class=''><a href='#'><span>Keys</span></a></li>
-	<li class='active'><a href='http://ucjeps.berkeley.edu/seaweedflora/seaweedflora/pages/californiaseaweeds.html'><span>Current Names</span></a></li>
-	<li class=''><a href="#">References</a></li>
-	<li class=''><a href="#">Glossary</a></li>
+	<li class='active'><a href='http://ucjeps.berkeley.edu/seaweedflora/'><span>Home</span></a></li>
+	<li class='has-sub'><a href='#'><span>Keys&#x25BE;</span></a>
+		<ul>
+			<li><a href="http://ucjeps.berkeley.edu/seaweedflora/green/index.html">Greens</a></li>
+        	<li><a href="http://ucjeps.berkeley.edu/seaweedflora/kelp/index.html">Kelps</a></li>
+        	<li><a href="http://ucjeps.berkeley.edu/seaweedflora/brown/index.html">Browns</a></li>
+		</ul>
+	</li>
+	<li class='active'><a href='http://ucjeps.berkeley.edu/seaweedflora/pages/californiaseaweeds.html'><span>Current Names</span></a></li>
+	<!--<li class=''><a href="#">References</a></li>-->
+	<li class=''><a href="http://ucjeps.berkeley.edu/seaweedflora/pages/glossary.html">Glossary</a></li>
 	<li class='has-sub'><a href='#'><span>More Resources &#x25BE;</span></a>
       <ul>
         <li><a href="http://ucjeps.berkeley.edu/INA.html">INA</a></li>
@@ -13,7 +19,8 @@
         <li><a href="http://www.algaebase.org">AlgaeBase</a></li>
       </ul>
    </li>
-	<li class='last'><a href="#">Art</a></li>    
+	<!--<li class='last'><a href="#">Art</a></li>  --> 
+	<li class='last'><a href="http://ucjeps.berkeley.edu/seaweedflora/pages/share.html">Share</a></li> 
 </ul>
 </div>
 
