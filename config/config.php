ed12c32237bf319b2f72234a13d6c2ea2f008371
jalexander <jason_alexander@berkeley.edu>
2017-08-15 09:35:16 -0700

    removed ignore from config.php, added config.php so can track changes on non-template file

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+//copy this file to config.php
+//and change the $config path to somewhere outside the site root
+//where you will store the eflora database and other DB configs
+$config_path = "../../ucjeps_data/seaweedflora/";
+$database_name = 'seaweedflora.db';
+$database_location = $config_path . $database_name;
+?>
