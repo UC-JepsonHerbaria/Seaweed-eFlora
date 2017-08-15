559983f027caf80ce5478f9c68f8ba2ce11ac1af
jalexander <jason_alexander@berkeley.edu>
2017-08-15 10:22:40 -0700

    updated link URLs

--- common/php/treatment_footer.php
+++ common/php/treatment_footer.php
@@ -8,9 +8,9 @@ date_default_timezone_set('America/Los_Angeles'); ?>
     <td height="20"><span class="copyrightText"><a href="http://ucjeps.berkeley.edu/main/copyright.html"> Copyright</a> &copy; <?php echo date("Y") ?>  Regents of the University of California
 <br>
 <b>Citation for this page:</b> <i><?php echo "$ScientificName" ?></i>, 
-in Kathy Ann Miller (ed.), <?php echo date("Y") ?> <em>California Seaweeds eFlora</em>, http://herbaria4.herb.berkeley.edu/seaweedflora/eflora_display.php?tid=<?php echo $URL_TaxonID?> [accessed on <?php echo date ("F d, Y")?>]
+in Kathy Ann Miller (ed.), <?php echo date("Y") ?> <em>California Seaweeds eFlora</em>, http://ucjeps.berkeley.edu/seaweedflora/eflora_display.php?tid=<?php echo $URL_TaxonID?> [accessed on <?php echo date ("F d, Y")?>]
 
-<br><b>Citation for the whole website:</b> Kathy Ann Miller (ed.) <?php echo date ("Y")?>. <em>California Seaweeds eFlora</em>,  http://ucjeps.berkeley.edu/seaweedflora.html [accessed on <?php echo date ("F d, Y")?>].
+<br><b>Citation for the whole website:</b> Kathy Ann Miller (ed.) <?php echo date ("Y")?>. <em>California Seaweeds eFlora</em>,  http://ucjeps.berkeley.edu/seaweedflora/ [accessed on <?php echo date ("F d, Y")?>].
 
 <br>
 <br>We encourage links to these pages, but the content may not be downloaded for reposting, repackaging, redistributing, or sale in any form, without written permission from The University Herbarium.
